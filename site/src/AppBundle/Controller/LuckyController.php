17ce7ef09a6c7118c84ed146c5df2dc2e090f549
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-20 21:18:20 +0100

    Symfony Discovering - LuckyController

--- site/src/AppBundle/Controller/LuckyController.php
+++ site/src/AppBundle/Controller/LuckyController.php
@@ -1,5 +1,7 @@
 <?php // src/AppBundle/Controller/LuckyController.php
 
+/* ----- ----- V1 - With render() (Templates) ----- ----- */
+
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,5 +20,53 @@ class LuckyController extends Controller
         return $this->render('lucky/number.html.twig', array(
             'number' => $number,
         ));
+    }
+}
+
+/* ----- ----- V1 - With $max (Routing) ----- ----- */
+
+//namespace AppBundle\Controller;
+//
+//use Symfony\Component\HttpFoundation\Response;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//
+//class LuckyController
+//{
+//    /**
+//     * @Route("/lucky/number/{max}")
+//     */
+//    public function numberAction($max)
+//    {
+//        $number = mt_rand(0, $max);
+//
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
+//    }
+//}
+
+/* ----- ----- V3 - With a controller ----- ----- */
+
+//namespace AppBundle\Controller;
+//
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Psr\Log\LoggerInterface;
+//
+//class LuckyController extends Controller
+//{
+//    /**
+//     * @Route("/lucky/number")
+//     */
+//    public function numberAction(LoggerInterface $logger)
+//    {
+//        $number = mt_rand(0, 100);
+//
+//        $logger->info('We are logging!');
+//
+//        return $this->render('lucky/number.html.twig', array(
+//            'number' => $number,
+//        ));
     }
 }
