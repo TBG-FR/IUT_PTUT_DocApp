1bc295735b6b429b19dbf8131a9087b7d460b6fe
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2018-01-22 14:58:04 +0100

    Appointments - Views/Controller : Appt related pages

--- site/src/AppBundle/Controller/AppointmentsController.php
+++ site/src/AppBundle/Controller/AppointmentsController.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Appointment;
 use AppBundle\Entity\Speciality;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Date;
 
 class AppointmentsController extends Controller
 {
@@ -23,6 +25,45 @@ class AppointmentsController extends Controller
         return $this->render(':appointments:results.html.twig', [
             'specialities' => $specialities,
             'extended' => true
+        ]);
+    }
+
+    /**
+     * @Route("/appt/{id}/details", name="appointments.details")
+     */
+    public function displayDetailsAction($id, Request $request)
+    {
+        $appointment = $this->getDoctrine()->getRepository(Speciality::class)->find($id);
+
+        return $this->render(':appointments:details.html.twig', [
+            'appointment' => $appointment
+            /*'extended' => true*/
+        ]);
+    }
+
+    /**
+     * @Route("/appt/{id}/reservation", name="appointments.reservation")
+     */
+    public function reserveApptAction($id, Request $request)
+    {
+        $appointment = $this->getDoctrine()->getRepository(Speciality::class)->find($id);
+
+        return $this->render(':appointments:reservation.html.twig', [
+            'appointment' => $appointment
+            /*'extended' => true*/
+        ]);
+    }
+
+    /**
+     * @Route("/appt/{id}/success", name="appointments.success")
+     */
+    public function successAction($id, Request $request)
+    {
+        $appointment = $this->getDoctrine()->getRepository(Speciality::class)->find($id);
+
+        return $this->render(':appointments:success.html.twig', [
+            'appointment' => $appointment
+            /*'extended' => true*/
         ]);
     }
 }
