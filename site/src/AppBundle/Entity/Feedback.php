bde4658eb078f76381c0d782cbe7e283aa31bffe
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2018-02-08 17:48:51 +0100

    New Functionality : Feedbacks
    - Added Controller, Repository, Entity and Views related to Feedback
    - Working pages (adding a feedback, successful add, listing feedbacks)
    - nto

---
+++ site/src/AppBundle/Entity/Feedback.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use UserBundle\Entity\User;
+
+/**
+ * Feedback
+ *
+ * @ORM\Table(name="feedback")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="date", type="date", nullable=false)
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", nullable=false)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="string", nullable=false)
+     */
+    private $content;
+
+    /* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */
+
+    public function __construct()
+    {
+        $this->setDate(new \DateTime());
+    }
+
+    /* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string $mail
+     */
+    public function setMail(string $mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content)
+    {
+        $this->content = $content;
+    }
+
+    /* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */
+
+}
+
